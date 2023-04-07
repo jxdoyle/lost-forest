33fbad2b878d4ee3e2d05aab4c351f505f92b1a5
jxdoyle <56358030+jxdoyle@users.noreply.github.com>
2023-04-07 14:58:01 +0100

    update files

--- src/options/gameConfig.js
+++ src/options/gameConfig.js
@@ -1,41 +1,23 @@
 // global game options
 const gameConfig = {
   drop: 1,
-
   dropForce: 150,
-
   platformSpeed: 300,
-
   spawnRange: [80, 300],
-
   platformSpawnRate: 5000,
-
   platformSizeRange: [150, 300],
-
   platformInitial: [450, 550],
-
   platformHeightRange: [350, 520],
-
   playerGravity: 900,
-
   jumpForce: 500,
-
   playerPositionX: 250,
-
   playerPositionY: 622,
-
   jumps: 2,
-
   scoreSpeed: 500,
-
   skeletonSpawnRate: 6000,
-
   spikeWidth: 56,
-
   spikeScaleRange: [1, 4],
-
   spikePercent: 25,
-
   spikeSpawnRate: 10000,
 };
 
