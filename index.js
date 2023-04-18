8c59fcf10cea1e1eabb2b92f5e8bb1dcb27886c7
(Student) - James Doyle <c00252855@ITCARLOW.IE>
2023-04-18 14:41:17 +0100

    removed pwa

--- index.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("Service Worker Registered!");
-        console.log(registration);
-    }).catch(error => {
-        console.log("Service Worker ");
-        console.log(error);
-    });
-}
